5315767f2231bdd67ec95a9110bdabb60e16687b
Fabian Muscariello <nroi@mailbox.org>
2019-03-24 13:42:38 +0100

    Add test case

--- src/test/kotlin/GraphTest.kt
+++ src/test/kotlin/GraphTest.kt
@@ -40,6 +40,22 @@ class GraphTest : StringSpec({
         }
     }
 
+    "kahn should work for graphs that are not fully connected" {
+        val n0 = Node(listOf(), "n0")
+        val n1 = Node(listOf(), "n1")
+        val n2 = Node(listOf(n1), "n2")
+        val n3 = Node(listOf(n1), "n3")
+        val n4 = Node(listOf(n2, n3), "n4")
+        for (permuted in permute(listOf(n0, n1, n2, n3, n4))) {
+            val sorted = Graph(permuted).kahn()
+            sorted.size shouldBe 5
+            sorted.indexOf(n1) shouldBeLessThan sorted.indexOf(n2)
+            sorted.indexOf(n1) shouldBeLessThan sorted.indexOf(n3)
+            sorted.indexOf(n2) shouldBeLessThan sorted.indexOf(n4)
+            sorted.indexOf(n3) shouldBeLessThan sorted.indexOf(n4)
+        }
+    }
+
     "test all getters" {
         val node = Node(listOf(), "a very nice name.").apply {
             payload shouldBe "a very nice name."
