0d5478aeb81d58fb4b1944c02e4eac4c70536daf
Fabian Muscariello <nroi@mailbox.org>
2019-03-24 16:21:43 +0100

    Minor fixes

--- src/main/kotlin/space/xnet/Main.kt
+++ src/main/kotlin/space/xnet/Main.kt
@@ -22,33 +22,32 @@ fun connect(url: String, user: String, password: String) {
 
     val connection = DriverManager.getConnection(url, user, password)
     connection.use { usedConnection ->
-        val statement = usedConnection.prepareStatement(matViewsQuery)
-        val resultSet = statement.executeQuery()
-        while (resultSet.next()) {
-            val schema = resultSet.getString(1)
-            val name = resultSet.getString(2)
+        val matViewsStatement = usedConnection.prepareStatement(matViewsQuery)
+        val matViewsResultSet = matViewsStatement.executeQuery()
+        while (matViewsResultSet.next()) {
+            val schema = matViewsResultSet.getString(1)
+            val name = matViewsResultSet.getString(2)
             views.add(MaterializedView(schema, name))
         }
-    }
 
-    val graph = array2dOfBoolean(views.size, views.size)
+        val graph = array2dOfBoolean(views.size, views.size)
 
-    connection.use { usedConnection ->
-        val statement = usedConnection.prepareStatement(matViewsDependenciesQuery)
-        val resultSet = statement.executeQuery()
-        while (resultSet.next()) {
-            val sourceSchema = resultSet.getString(1)
-            val sourceName = resultSet.getString(2)
-            val destinationSchema = resultSet.getString(3)
-            val destinationName = resultSet.getString(4)
+        val dependenciesStatement = usedConnection.prepareStatement(matViewsDependenciesQuery)
+        val dependenciesResultSet = dependenciesStatement.executeQuery()
+        while (dependenciesResultSet.next()) {
+            val sourceSchema = dependenciesResultSet.getString(1)
+            val sourceName = dependenciesResultSet.getString(2)
+            val destinationSchema = dependenciesResultSet.getString(3)
+            val destinationName = dependenciesResultSet.getString(4)
             val source = MaterializedView(sourceSchema, sourceName)
             val destination = MaterializedView(destinationSchema, destinationName)
             val idxSource = views.indexOf(source)
             val idxDestination = views.indexOf(destination)
             graph[idxSource][idxDestination] = true
         }
+
+        val kahn = kahnFromArray(graph, views)
+        println("dependencies: " + kahn)
     }
 
-
-    println("views: " + views)
 }
