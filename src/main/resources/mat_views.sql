3a727dce4370ec65c70b16c7806f7b042f68102b
Fabian Muscariello <nroi@mailbox.org>
2020-08-29 10:08:39 +0200

    Reformat SQL file

--- src/main/resources/mat_views.sql
+++ src/main/resources/mat_views.sql
@@ -1,9 +1,6 @@
-select
-ns.nspname as mat_view_schema,
-relname as mat_view
-from
-pg_class pgc
-inner join pg_namespace ns on
-pgc.relnamespace = ns.oid
-where
-pgc.relkind = 'm'
+select ns.nspname as mat_view_schema,
+       relname    as mat_view
+from pg_class pgc
+         inner join pg_namespace ns on
+    pgc.relnamespace = ns.oid
+where pgc.relkind = 'm'
